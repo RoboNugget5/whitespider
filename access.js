a81e4538230deccda77ea360db885b645b89b246
RoboNugget5 <152231652+RoboNugget5@users.noreply.github.com>
2024-06-10 19:24:42 -0400

    Update access.js

--- access.js
+++ access.js
@@ -1,16 +1,1 @@
-const whitelist = ["68.132.119.75"];
 
-
-$(document).ready(()=>{
-    // Use "https://ipinfo.io" link to use the
-    // ipinfo for getting the ip address
-    $.getJSON("https://ipinfo.io",
-    function (response) {
-        
-        if ((whitelist.includes(response.ip)) == false) {
-            document.getElementById("html").innerHTML = "<h1>Sorry, you do not have access to this page.</h1>";
-            console.log("hi");
-            
-        }
-    });
-})  
