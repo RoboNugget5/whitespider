d506369e8697a0766f4d9fcbcafbc0caa5d14701
RoboNugget5 <152231652+RoboNugget5@users.noreply.github.com>
2024-06-12 16:58:52 -0400

    Update access.js

--- access.js
+++ access.js
@@ -1,6 +1,20 @@
-const whitelist = ["68.132.119.75"];
+const whitelist = ["65.132.119.75"];
+
+var ip = "ghgyhgbgbghvbggb";
 
 
+setTimeout(() => {
+    check();
+  }, 1000);
+
+function check() {
+    if ((whitelist.includes(ip)) == false) {
+        
+        document.getElementById("body").innerHTML = "<h1>Sorry, you do not have access to this page.</h1>";
+        console.log("hi");
+    }
+    
+}
 $(document).ready(()=>{
     // Use "https://ipinfo.io" link to use the
     // ipinfo for getting the ip address
@@ -8,11 +22,7 @@ $(document).ready(()=>{
     function (response) {
         console.log(response.ip);
         console.log("jfh")
+        ip = response.ip;
         
-        if ((whitelist.includes(response.ip)) == false) {
-            document.getElementById("body").innerHTML = "<h1>Sorry, you do not have access to this page.</h1>";
-            console.log("hi");
-            
-        }
     });
 })  
